86cc5bf29b2c84051df04df6c296a5eaa5218d8c
evgeniy_prokofyev <prokofyev.evg@yandex.ru>
2020-06-29 15:44:30 +0300

    домашнее задание ко второму уроку

--- js/main.js
+++ js/main.js
@@ -1,28 +1,69 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+class ShoppingCart{
+    constructor(items) { /*  */ }
+    addItem(ShoppingCartItem) {    /* Добавить товар*/ }
+    removeItem(ShoppingCartItem) {    /* Удалить товар*/ }
+}
 
-const renderProduct = (title, price, img=`https://via.placeholder.com/290?text=${title}`) => {
-    return `<div class="product-item">
-                <img src="${img}" alt="">
-                <div class="product-text">
-                    <div class="product-heading">
-                        <h3 class="product-name">${title}</h3>
-                        <p class="product-price">${price}</p>
+class ShoppingCartItem{
+    constructor(name, price) { /*  */ }
+    incCount() {    /* Добавить добавку */}
+}
+
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+        this.render();
+    }
+
+    _fetchProducts() {
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObject = new ProductItem(product);
+            this.allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+
+    calcSum()
+    {
+        return this.goods.reduce((sum, item) => sum + item.price, 0);
+    }
+}
+
+class ProductItem {
+    constructor(product, img=`https://via.placeholder.com/290?text=${product.title}`) {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                    <img src="${this.img}" alt="">
+                    <div class="product-text">
+                        <div class="product-heading">
+                            <h3 class="product-name">${this.title}</h3>
+                            <p class="product-price">${this.price}</p>
+                        </div>
+                        <button class="button card-button">
+                            <span class="button-text">Добавить в корзину</span>
+                        </button>
                     </div>
-                    <button class="button card-button">
-                        <span class="button-text">Добавить в корзину</span>
-                    </button>
-                </div>
-              </div>`;
-};
+                </div>`;
+    }
+}
 
-const renderProducts = (list) => {
-    const productList =  list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList.join('');
-};
-
-renderProducts(products);
+const pList = new ProductList();
