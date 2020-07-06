e7c7482e9b324bf36b34fd3181b8dfc855b2b18a
Evgeniy Prokofyev <prokofyev.Evg@yandex.ru>
2020-07-06 13:47:40 +0300

    Домашнее задание к пятому уроку

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price, img=`https://via.placeholder.com/290?text=${title}`) => {
-    return `<div class="product-item">
-                <img src="${img}" alt="">
-                <div class="product-text">
-                    <div class="product-heading">
-                        <h3 class="product-name">${title}</h3>
-                        <p class="product-price">${price}</p>
-                    </div>
-                    <button class="button card-button">
-                        <span class="button-text">Добавить в корзину</span>
-                    </button>
-                </div>
-              </div>`;
-};
-
-const renderProducts = (list) => {
-    const productList =  list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList.join('');
-};
-
-renderProducts(products);
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        goodsList: [],
+        imgCatalog: 'https://placehold.it/290',
+        isVisibleCart: false,
+        searchLine : ''
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+            this.goodsList.push(product);
+        },
+        filterGoods(){
+            console.log(this.searchLine);
+        },
+        toggleCart(){
+            this.isVisibleCart = !this.isVisibleCart; 
+        },
+    },
+    computed: {
+        cartPrice: function () {
+            return this.goodsList.reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
+        }
+    },
+    created() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                }
+            });
+    },
+});
